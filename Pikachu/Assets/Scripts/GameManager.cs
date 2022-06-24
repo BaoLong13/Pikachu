04d2b4207c0cfd824ac842b0b3f18c73851770a9
00111011 <MadTrickster13@gmail.com>
2022-06-24 21:14:12 +0700

    (WIP) GameManager

--- Pikachu/Assets/Scripts/GameManager.cs
+++ Pikachu/Assets/Scripts/GameManager.cs
@@ -4,15 +4,46 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public static GameManager instance;
+    public GameState gameState;
+
+    private void Awake()
     {
-        
+        instance = this;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
-        
+        ChangeState(GameState.GenerateGrid);
     }
+
+    public void ChangeState(GameState newState)
+    {
+        gameState = newState;
+        switch (newState)
+        {
+            case GameState.GenerateGrid:
+                break;
+            case GameState.SpawnBall:
+                break;
+            case GameState.SpawnQueueBall:
+                break;
+            case GameState.StartTurn:
+                break;
+            case GameState.EndTurn:
+                break;
+            case GameState.Lose:
+                break;
+        }
+    }
+}
+
+public enum GameState
+{
+    GenerateGrid = 0,
+    SpawnBall = 1,
+    SpawnQueueBall = 2,
+    StartTurn = 3,
+    EndTurn = 4,
+    Lose = 5
 }
